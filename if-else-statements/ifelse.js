3c754f57c1287603808e0073e3df93f874bba906
unknown <eric.price121@gmail.com>
2023-08-17 20:59:16 -0700

    if else submission

---
+++ if-else-statements/ifelse.js
@@ -0,0 +1,10 @@
+let hour = 22;
+
+if (hour >= 6 && hour < 12) {
+  console.log('Good Morning fool!');
+}
+else if (hour <= 12 && hour >= 18) {
+  console.log('Good Afternoon goofy!');
+}
+else
+  console.log('Good Evening weirdo!');
